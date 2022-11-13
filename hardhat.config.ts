4821266fcacd91ff67dd423dbf584f8da96df094
kyzooghost <jeffersonzjtang@gmail.com>
2022-11-13 23:17:05 +1100

    fixed dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
+import { config as dotenv_config } from 'dotenv';
+dotenv_config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
+  defaultNetwork: 'goerli',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      chainId: 5,
+      accounts: JSON.parse(process.env.PRIVATE_KEYS || '[]'),
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
